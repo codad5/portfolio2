372791d09df2d36ee602c0cf87f2e6bca41b7df8
cadad5 <aniezeoformic@gmail.com>
2023-02-16 13:59:49 +0000

    final upgrade

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { ReactHTMLElement, useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import GithubLogo from './assets/Github.svg'
@@ -327,6 +328,36 @@ function App() {
           </div>
         </div>
       </div> */}
+=======
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import DropSection from './components/DropSection'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      
+      <div className="accordion" id="Main">
+        <DropSection title={"Home"} expanded={true}>
+            <div className='w-full'>
+              Hello
+            </div>
+        </DropSection>
+        <DropSection title={"About"}>
+            <div className='w-full'>
+              Hello
+            </div>
+        </DropSection>
+        <DropSection title={"Projects"}>
+            <div className='w-full'>
+              Hello
+            </div>
+        </DropSection>
+      </div>
+>>>>>>> db4b2c4 (error merge)
     </div>
   )
 }
