8c45cc78c7201c2b04eb971caa534fa8c58b075b
cadad5 <aniezeoformic@gmail.com>
2023-02-16 13:11:06 +0000

    added resume

--- src/components/Wordcard.tsx
+++ src/components/Wordcard.tsx
@@ -3,7 +3,7 @@ import { Children } from "react";
 export default function WorkCard({ title, timeline, position, children , href}: { title: string, timeline: string, position: string, children?: JSX.Element, href ?: string }){
     return (
         <div className="w-[80%] text-left">
-            <a  href={href} ><h5 className="text-xl font-black underline">{title}</h5></a>
+            <a  href={href} ><h5 className="text-xl font-black underline hover:text-red">{title}</h5></a>
             <h6 className="font-semibold italic">{position}</h6>
             <span>{timeline}</span>
             <div className="w-full px-5 py-3">
