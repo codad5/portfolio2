da2ac763ddece3a314b01a74e283b71cfe22d12c
codad5 <aniezeoformic@gmail.com>
2023-03-06 00:19:22 +0000

    i

--- src/App.tsx
+++ src/App.tsx
@@ -219,7 +219,7 @@ function App() {
             delaySpeed={1000}
           /></div>
         </footer>
-        <a className='fixed bottom-10 right-14 z-50' href='#footername'>
+        <a className='fixed bottom-10 right-10 z-50' href='#footername'>
           <div className="animate-bounce bg-white dark:bg-slate-800 p-2 w-10 h-10 ring-1 ring-slate-900/5 dark:ring-slate-200/20 shadow-lg rounded-full flex items-center justify-center">
             <svg className="w-6 h-6 text-violet-500" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
               <path d="M19 14l-7 7m0 0l-7-7m7 7V3"></path>
