8f18aa2252d43f7c7ca44cf77822cacf4340f021
codad5 <aniezeoformic@gmail.com>
2023-03-20 12:31:30 +0000

    Update

---
+++ app/posts/page.tsx
@@ -0,0 +1,33 @@
+import Link from "next/link";
+import { getPostsAndMetadata } from "../helpers/post";
+
+export default function PostLayout() {
+    const posts = getPostsAndMetadata();
+    return (
+        <>
+            {
+                posts.length > 0 ? (
+                    <>
+                        
+                        <div className='w-full p-4 text-center  '>
+                            <ul className='list-disc w-full md:w-4/5'>
+                                {posts.map((value, index) => (
+                                    <li key={index} className='w-4/5 text-left border-1 py-4'>
+                                        <div>
+                                            <Link href={`posts/${value.slug}`} className='underline text-lg font-bold'>{value.title}</Link>
+                                            <p className='text-sm'>Date: {value.date}</p>
+                                            <p className='text-sm'>Tags: {value.tags}</p>
+                                        </div>
+
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                    </>
+                ) : <div className='w-full grid place-items-center h-[50vh]'>
+                    <b className='text-3xl'> <h2> Coming Soon</h2></b>
+                </div>
+            }
+        </>
+    )
+}
