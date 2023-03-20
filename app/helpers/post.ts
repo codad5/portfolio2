4397da38edc26503a985327100c3576a61594381
codad5 <aniezeoformic@gmail.com>
2023-03-20 11:09:22 +0000

    SEO

--- app/helpers/post.ts
+++ app/helpers/post.ts
@@ -27,6 +27,20 @@ export const getPostsAndMetadata = () : postsType[] => {
 }
 
 
+export const getPostMetadata = (slug : string): postsType => {
+    const fileContent = fs.readFileSync(`posts/${slug}.md`, 'utf-8')
+    const { data, content } = matter(fileContent)
+    return {
+        slug: slug.replace('.md', ''),
+        title: data.title,
+        date: data.date,
+        description: data.description ?? content.slice(0, 60),
+        tags: data.tags,
+        image: data.image
+    }
+}
+
+
 export const getPostContent = (slug: any) => {
     //get file contents
     return fs.readFileSync(`posts/${slug}.md`, 'utf-8')
