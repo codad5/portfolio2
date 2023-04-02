ea5e373b9f2753db40ae60b005cefc8200ff5401
codad5 <aniezeoformic@gmail.com>
2023-04-02 03:58:26 +0000

    push

---
+++ app/helpers/highlighter.tsx
@@ -0,0 +1,12 @@
+"use client"
+import hljs from "highlight.js";
+import { useEffect } from "react";
+import Highlight from "react-highlight";
+hljs.autoDetection("markdown")
+
+export default function HighlightHelper(){
+    useEffect(() => {
+        hljs.initHighlighting();
+    }, [])
+    return null;
+}
